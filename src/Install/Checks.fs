a90a249fe82a76b7e04b10ccc3877164b984b1b8
omaus <omaus@bio.uni-kl.de>
2021-07-01 17:00:29 +0200

    Add new folder structure + naming

---
+++ src/Install/Checks.fs
@@ -0,0 +1,58 @@
+﻿module Checks
+
+open FSharp.Data
+
+type ParseResult<'a> = {
+    Result  : 'a
+    Success : bool
+}
+
+let getParseResult res = {
+    Result  = res
+    Success = res <> ""
+}
+
+[<Literal>]
+let SwateUrl = @"https://swate.denbi.uni-tuebingen.de/"
+/// Can be used to check Client - Server connection
+
+[<Literal>]
+let GetAppVersionAPI = SwateUrl + @"api/IServiceAPIv1/getAppVersion"
+/// Can be used to check Server - Database connection
+
+[<Literal>]
+let GetAllOntologiesAPI = SwateUrl + @"api/IAnnotatorAPIv1/getAllOntologies"
+
+let getAppVersion () = 
+    getParseResult (
+        try FSharp.Data.Http.RequestString(GetAppVersionAPI)
+        with err -> Console.error <| sprintf "ERROR: Did not get App version due to:\n%s" err.Message; ""
+    )
+
+let checkSemVer appVer =
+    let pattern = @"""(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"""
+    match System.Text.RegularExpressions.Regex.Match(appVer, pattern) with
+    | x when x.Success -> Console.ok <| sprintf "AppVer %s is a SemVer" appVer
+    | _ -> Console.error <| sprintf "ERROR: AppVer %s is not a SemVer" appVer
+
+type Ontology = {
+    Name            : string
+    CurrentVersion  : string
+    Definition      : string
+    DateCreated     : string
+    UserID          : string
+}
+
+let getAllOntologies () =
+    getParseResult (
+        try FSharp.Data.Http.RequestString(GetAllOntologiesAPI) 
+        with err -> Console.error <| sprintf "ERROR: Was not able to parse ontologies due to:\n%s" err.Message; ""
+    )
+    
+let getOntoList jsonedFullstring = Newtonsoft.Json.JsonConvert.DeserializeObject<Ontology list> jsonedFullstring
+
+let checkOnto onto =
+    let checkDate () =
+        try onto.DateCreated |> System.DateTime.Parse |> fun date -> printfn "Ontology %s has System.DateTime (%A) in DateCreated" onto.Name date
+        with err -> Console.error <| sprintf "ERROR: Ontology %s misses a System.DateTime in DateCreated" onto.Name
+    checkDate ()
---
+++ src/Install/Program.fs
@@ -0,0 +1,38 @@
+﻿open Checks
+
+let checkParseResult (res : Checks.ParseResult<'a>) =
+    if res.Success = false 
+    then
+        printfn "\nSwateCheck was unsuccessful. Press any key to close the app."
+        System.Console.ReadKey() |> ignore
+        System.Environment.Exit(0)
+
+
+[<EntryPoint>]
+let main argv =
+    Console.info "Installing Swate."
+
+    // Download part
+    Install.downloadMain ()
+    
+    // Install part
+    Install.installMain ()
+
+    // Check part
+    Console.info "Check connectivity."
+
+    let appVer = Checks.getAppVersion ()
+    checkParseResult appVer
+    Checks.checkSemVer appVer.Result
+
+    let allOntos = Checks.getAllOntologies ()
+    checkParseResult allOntos
+    let allOntosJsoned = Checks.getOntoList allOntos.Result
+    
+    List.iter Checks.checkOnto allOntosJsoned
+    Console.ok "SwateCheck successful."
+
+    Console.ok "Swate is installed and can be accessed, after an Excel restart. Press any key to close the app"
+    System.Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
