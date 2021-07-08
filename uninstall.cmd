bf1064c6c2c85d66131c714d55c5dd4b2f22fe6a
omaus <omaus@bio.uni-kl.de>
2021-07-08 11:48:20 +0200

    Update Uninstallers

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,5 +1,13 @@
+@echo off
+
+echo Deleting Swate
 .\config\Set-WebAddin.exe -cleanup -manifestPath ".\config\manifest.xml"
 
+echo Deleting manifest.xml, deleting WebAddin-sideloader
 del ".\config\manifest.xml" ".\config\Set-WebAddin.exe"
 
+echo Deleting config folder
 rmdir ".\config"
+
+echo Uninstalling done. Press any key to exit.
+pause >nul
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,7 @@
+echo Deleting Swate
 ./config/Set-WebAddin.exe -cleanup -manifestPath "./config/manifest.xml"
 
-del "./config/manifest.xml" "./config/Set-WebAddin.exe"
+echo Deleting manifest.xml, deleting WebAddin-sideloader, deleting config folder
+rm -r ./config/
 
-rmdir "./config"
+read -n1 -r -p "Uninstalling done. Press any key to exit." key
