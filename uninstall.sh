2a7b9fce33d7e2b1eea21092592fc4f2553e27ab
omaus <omaus@bio.uni-kl.de>
2021-10-04 16:20:42 +0200

    Delete deprecated uninstall shell script

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-echo Deleting Swate
-./config/Set-WebAddin.exe -uninstall -installedManifestFullname "./config/sideloaderData/manifest.xml"
-
-echo Deleting manifest.xml, deleting WebAddin-sideloader, deleting config folder
-rm -r ./config/
-
-read -n1 -r -p "Uninstalling done. Press any key to exit." key
