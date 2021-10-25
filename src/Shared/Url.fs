4d20d88d131271ad5496da04b1958c32733360e4
omaus <omaus@bio.uni-kl.de>
2021-10-25 16:21:13 +0200

    Update URLs to match Swate v0.5.0

--- src/Shared/Url.fs
+++ src/Shared/Url.fs
@@ -4,7 +4,7 @@ open System
 open System.IO
 
 [<Literal>]
-let SwateUrl = @"https://swate.denbi.uni-tuebingen.de/"
+let SwateUrl = @"https://swate.nfdi4plants.de/"
 
 /// Can be used to check Client - Server connection
 [<Literal>]
@@ -15,8 +15,7 @@ let GetAppVersionAPI = SwateUrl + @"api/IServiceAPIv1/getAppVersion"
 let GetAllOntologiesAPI = SwateUrl + @"api/IAnnotatorAPIv1/getAllOntologies"
 
 [<Literal>]
-// let ManifestUrl = @"https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/manifest.xml" // atm. outdated, uncomment when next version is live
-let ManifestUrl = @"https://raw.githubusercontent.com/nfdi4plants/Swate/47965986e914f30b3b438bc44ede81308dc16d39/.assets/assets/manifest.xml"
+let ManifestUrl = @"https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/manifest.xml"
 
 [<Literal>]
 let SideloaderUrl = @"https://github.com/davecra/WebAddinSideloader/raw/master/Set-WebAddin%20(v1.0.0.1).zip"
